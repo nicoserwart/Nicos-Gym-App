d56d64843c3fbd465a944fe9bb841aa05c926484
nicoserwart <nicoserwart@gmail.com>
2026-04-06 15:49:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'nicos-gym-app-v2';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    }).catch(err => {
+      console.warn('Cache addAll failed:', err);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
